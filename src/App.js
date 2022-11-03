f1d7352bc28aeb9190ab31669bb4c72a2112fb5a
Jayden Owens <77424127+Jayden-Owens@users.noreply.github.com>
2022-11-02 23:09:19 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Menu from './components/Menu';
+import NewsGrid from './components/NewsGrid';
+import './App.css'
 
 function App() {
+  const [items, setItems] = useState([])
+  const [active, setActive] = useState(1)
+  const [category, setCategory] = useState("general")
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=b95173f944324e94b9022e8d7ec646eb`)
+    .then(res => res.json())
+    .then(data => setItems(data.articles))
+}, [category])
+  console.log(items)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">See The Latest News</h1>
+      <Menu active={active} setActive={setActive} setCategory={setCategory}/>
+      <NewsGrid items={items}/>
     </div>
-  );
+  )
 }
 
 export default App;
