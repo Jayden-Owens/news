f1cfa93540bf234699f46de410e97629633f3951
Jayden Owens <77424127+Jayden-Owens@users.noreply.github.com>
2022-11-02 23:54:50 -0600

    initial

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,21 +1,18 @@
 import '../App.css'
 
 function NewsItem({item}) {
-    const websiteUrl = item.url
-    const website = websiteUrl.split("https://").pop().split('/')[0]
-
     const date = item.publishedAt
     const formatDate = date.replace('T', '')
     const formatTime = formatDate.replace('Z', '')
 
     return(
-        <a href="" className="article">
+        <a href={item.url} className="article">
             <div className="article-image">
                 <img src={item.urlToImage} alt={item.title}/>
             </div>
             <div className="article-content">
                 <div className="article-source">
-                    <img src={`https://t2.gstatic.com/faviconv2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://${website}&size=16`} alt={item.source.id} />
+                    
                     <span>{item.source.name}</span>
                 </div>
                 <div className="article-title">
